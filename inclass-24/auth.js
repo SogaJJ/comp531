f47dd470fe83cf60f9f36c592de4620d7ee6493b
SogaJJ <tju.jguo@gmail.com>
2016-11-15 22:17:06 -0600

    add inclass-24

---
+++ inclass-24/auth.js
@@ -0,0 +1,193 @@
+"use strict";
+
+
+var redis = require('redis').createClient("redis://h:p5q732uj3cnmt73ihj9848vcipe@ec2-23-23-247-182.compute-1.amazonaws.com:10789")
+
+var request = require('request')
+var qs = require('querystring')
+var express = require('express')
+var cookieParser = require('cookie-parser')
+var session = require('express-session')
+var passport = require('passport')
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+// facebook Oauth
+const clientSecret = '0ed7ea0892bb42bfa48094a96b7b5a1e'
+const clientID = '196939974094911'
+const callbackURL = 'http://localhost:3000/auth/callback'
+const config = { clientSecret, clientID, callbackURL }
+
+passport.serializeUser(function(user, done) {
+	var user = users[id]
+	done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done) {
+	var user = users[id]
+	done(null, user)
+})
+
+passport.use(new FacebookStrategy(config,
+	function(token, refreshToken, profile, done) {
+		process.nextTick(function() {
+			return done(null, profile)
+		})
+	})
+)
+
+
+
+
+
+
+var cookieParser = require('cookie-parser')
+const md5 = require('md5')
+
+var pepper = md5('This is my secret pepper')
+
+const saltGenerator = (username) => {
+	return Math.floor(Math.random() * 10000000000) + username
+}
+
+// to serve as DB for in-class exercise
+var inmemoryDB = {
+	'jg37test': {
+		"username": "jg37test",
+		"salt": "6897698697jg37test",
+		"hash": "f11d3aa594f8dd988ccff1f3da4937f8"
+	}
+}
+
+
+function register(req, res) {
+	var username = req.body.username
+	var password = req.body.password
+	if (!username || !password) {
+		res.sendStatus(400)
+		return
+	}
+	var salt = saltGenerator(username)
+	var store = {
+		username: username,
+		salt: salt,
+		hash: md5(password + salt + pepper)
+	}
+	inmemoryDB[username] = store
+	var msg = {username: username, status: 'success'}
+	res.send(msg)
+}
+
+var cookieKey = 'sid'
+function login(req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+	if (!username || !password) {
+		res.sendStatus(400)
+		return
+	}
+	var store = inmemoryDB[username]
+	if (!store) {
+		res.sendStatus(401)
+		return 
+	}
+	var salt = store.salt
+	var hash = store.hash
+	if (md5(password + salt + pepper) !== hash){
+		res.sendStatus(401)
+		return 
+	}
+
+	var cookieSID = md5(username)
+	redis.hmset(cookieSID, {username})
+	res.cookie(cookieKey, cookieSID, {maxAge: 3600*1000, httpOnly: true})
+	var msg = {username: username, result: 'success'}
+	res.send(msg)
+}
+
+
+function password(req, res) {
+	res.status(200).send({
+		username: 'jg37test',
+		status: 'will not change'
+	})
+}
+
+function stubLogin(req, res){
+	res.status(200).send({
+		username: 'jg37test',
+		status: 'success'
+	})
+}
+
+function stubLogout(req, res){
+	res.status(200).send({
+		username: 'jg37test',
+		status: 'success'
+	})
+}
+
+function stubRegister(req, res){
+	res.status(200).send({
+		username: 'jg37test',
+		status: 'success'
+	})
+}
+
+
+const logout = (req, res) => {
+	var sid = req.cookies[cookieKey]
+	redis.del(sid)
+	res.clearCookie(cookieKey)
+	res.send('successfully logout')
+}
+
+const isLoggedIn = (req, res, next) => {
+	console.log(req.username)
+	console.log(req.cookies)
+	var sid = req.cookies[cookieKey]
+	redis.hgetall(sid, function(err, userObj) {
+		console.log(sid + 'mapped to ' + userObj.username)
+		if (userObj.username) {
+			next()
+		} else {
+			res.sendStatus(401)
+		}
+	})
+}
+
+
+
+const fail = (req, res) => {
+	res.send('fail')
+}
+
+const hello = (req, res) => {
+	res.send('hello world from Jing!')
+}
+
+function profile(req,res){
+	res.send({'ok now what?':req.user})
+}
+
+
+module.exports = app => {
+	app.use(session({secret: 'asdf'}));
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use('/login/facebook', passport.authenticate('facebook', { scope: 'email' }))
+	app.use('/callback', passport.authenticate('facebook', {
+		successRedirect: '/profile', failureRedirect: '/fail'
+	}))
+	app.use('/hello', hello)
+	app.use('/profile',profile)
+	app.use('/fail', fail)
+	// app.use('/logout', logout)
+	
+
+
+	app.use(cookieParser())
+
+	app.post('/login', login)
+	app.put('/logout', isLoggedIn, logout)
+	app.post('/register', register)
+}
